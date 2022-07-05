b56645e8ee765893c2084673ed9fec6304f74912
Mariana.Arroyo <arroyo.mb@gmail.com>
2022-07-05 11:07:06 -0300

    ejercicio entregable

---
+++ mi_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+from mi_app.models import Familiares
+
+
+
+def listar_familiares(request):
+
+    context = {
+        familiares: Familiares.objects.all(),
+    }
+    return render(request, "mi_app/familiares.html", context)
